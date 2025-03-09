1f3d9cf8ac10c51109623fc086bf8d29565f0d58
Oleksandra <sunnyem@seznam.cz>
2025-03-09 21:10:38 +0100

    icons

--- src/components/PilotProfile/PilotProfile.jsx
+++ src/components/PilotProfile/PilotProfile.jsx
@@ -1,5 +1,6 @@
 // export default function PilotProfile(props){
 // const { name, rank, age, spec } = props.pilot
+import { BsAirplaneFill, BsBackpack2 } from "react-icons/bs";
 import clsx from "clsx";
 import css from "./PilotProfiole.module.css";
 
@@ -11,10 +12,16 @@ export default function PilotProfile({
   return (
     <div>
       <p className={css.text}>Name:{name}</p>
-      <p className={css.text}>Rank:{rank}</p>
+      <p className={css.text}>
+        <BsAirplaneFill className={css.icon} />
+        Rank:{rank}
+      </p>
       <p className={css.text}>Age:{age}</p>
       <p className={css.text}>Speciality: {spec}</p>
-      <p className={statusCslx}>Active : {active ? "Active" : "Retired"}</p>
+      <p className={statusCslx}>
+        <BsBackpack2 className={css.icon} />
+        Active : {active ? "Active" : "Retired"}
+      </p>
     </div>
   );
 }
