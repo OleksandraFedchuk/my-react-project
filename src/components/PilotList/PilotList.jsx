ebb20a390bc9d81b83b3345e969b47dfe4637fc3
Oleksandra <sunnyem@seznam.cz>
2025-03-09 15:38:21 +0100

    Active&retired

--- src/components/PilotList/PilotList.jsx
+++ src/components/PilotList/PilotList.jsx
@@ -1,12 +1,12 @@
 import PilotProfile from "../PilotProfile/PilotProfile";
+import css from "./PilotList.module.css";
 
 export default function PilotList({ items }) {
   return (
-    <ul>
+    <ul className={css.list}>
       {items.map((officer) => (
-        <li key={officer.id}>
+        <li key={officer.id} className={css.item}>
           <PilotProfile pilot={officer} />
-          <hr />
         </li>
       ))}
     </ul>
--- src/components/PilotProfile/PilotProfile.jsx
+++ src/components/PilotProfile/PilotProfile.jsx
@@ -1,16 +1,20 @@
 // export default function PilotProfile(props){
 // const { name, rank, age, spec } = props.pilot
+import clsx from "clsx";
+import css from "./PilotProfiole.module.css";
 
 export default function PilotProfile({
   pilot: { name, rank, age, spec, active },
 }) {
+  const statusCslx = clsx(css.text, active ? css.active : css.retired);
+
   return (
     <div>
-      <p>Name:{name}</p>
-      <p>Rank:{rank}</p>
-      <p>Age:{age}</p>
-      <p>Speciality: {spec}</p>
-      <p>Active : {active ? "Active" : "Retired"}</p>
+      <p className={css.text}>Name:{name}</p>
+      <p className={css.text}>Rank:{rank}</p>
+      <p className={css.text}>Age:{age}</p>
+      <p className={css.text}>Speciality: {spec}</p>
+      <p className={statusCslx}>Active : {active ? "Active" : "Retired"}</p>
     </div>
   );
 }
